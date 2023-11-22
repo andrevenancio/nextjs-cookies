5f5f15964459164c9a876e99aeddc28058d23012
andrevenancio <info@andrevenancio.com>
2023-11-22 22:03:30 +0000

    removing env variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,10 @@ import { GetServerSidePropsContext } from "next"
 import { withSecureRoutes } from "../components"
 
 export default function Home() {
-  return <div>this is a protected page</div>
+  return <div>This is my homepage</div>
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  console.log("index get server side props")
   const secureRoute = await withSecureRoutes(context)
 
   return {
